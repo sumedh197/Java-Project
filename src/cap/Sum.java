9873b3cd501b7793845c87a28c28579db9c53fd4
unknown <sumedhtest2@gmail.com>
2025-04-28 10:47:14 +0530

    added new file

---
+++ src/cap/Sum.java
@@ -0,0 +1,19 @@
+package cap;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 10;
+		
+		int b = 20;
+		
+		int c;
+		
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
